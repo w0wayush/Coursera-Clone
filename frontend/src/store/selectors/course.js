901ba02a05051a22e5dbcece1abfa405eacce346
Ayush Pandey <113545732+w0wayush@users.noreply.github.com>
2024-06-05 04:19:31 +0530

    Update course.js

--- frontend/src/store/selectors/course.js
+++ frontend/src/store/selectors/course.js
@@ -31,6 +31,17 @@ export const courseTitle = selector({
   },
 });
 
+export const courseDescription = selector({
+  key: "courseDescriptionState",
+  get: ({ get }) => {
+    const state = get(courseState);
+    if (state.course) {
+      return state.course.description;
+    }
+    return "";
+  },
+});
+
 export const coursePrice = selector({
   key: "coursePriceState",
   get: ({ get }) => {
